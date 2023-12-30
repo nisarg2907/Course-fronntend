911cee12c95e5c9711e7f3e5d33f843fdd3bfa17
Nisarg <nisargshah.ict19@gmail.com>
2023-12-30 16:33:05 +0530

    made both basic frontend and backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
+const CourseCard = ({ course }) => {
+  const { id, author, name, price } = course;
+
+  return (
+    <div className="course-card">
+      <div>hii</div>
+      <h3>{name}</h3>
+      <p>Author: {author}</p>
+      <p>Price: ${price}</p>
+    </div>
+  );
+};
+
 function App() {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/courses');
+        setCourses(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []); // Empty dependency array to run the effect only once on mount
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {courses.map((course) => (
+        <CourseCard key={course.id} course={course} />
+      ))}
     </div>
   );
 }
